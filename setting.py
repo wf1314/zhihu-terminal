b14cbf7252b96421e04eb859d4e46889dcd2ce87
王凡 <wangfan@botpy.com>
2019-07-19 17:40:30 +0800

    增加配置项

--- setting.py
+++ setting.py
@@ -1,10 +1,14 @@
-
+"""
+路径使用绝对路径
+"""
 DEBUG = False
 
-LOG_DIR = ''  # df: /tmp/zhihu
+LOG_DIR = ''  # df: /tmp/zhihu  debug模式下存放日志
 
-COOKIE_FILE = ''  # df: /static/cookies.pick
+COOKIE_FILE = ''  # df: /tmp/cookies.pick   缓存的cookie文件
 
-USER = ''
+SAVE_DIR = ''  # df: /tmp/zhihu_save   保存回答到本地
 
-PASSWORD = ''
+USER = ''  # 必填  账号
+
+PASSWORD = ''   # 必填  密码
